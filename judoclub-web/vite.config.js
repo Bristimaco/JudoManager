aa9357c02c2bf50a48f594bdd9bc3cefe95e787b
Stijn Mattheus <stijn.mattheus@profunctional.be>
2026-03-18 09:46:21 +0100

    Set base path for Vite build to match routing rule

--- judoclub-web/vite.config.js
+++ judoclub-web/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/judomanager-judoclub-web/",
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
